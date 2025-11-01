d3dcf5030e2bc319fa15f08434225601afe7a290
Ajibode Olayinka <ajibodeolayinka09@gmail.com>
2025-11-01 22:21:11 +0100

    file handling problems:copying a file(Byte streams)

--- src/FileHandling/Ex4.java
+++ src/FileHandling/Ex4.java
@@ -6,8 +6,11 @@ import java.io.IOException;
 
 public class Ex4 {
 
-    public static void writerFunc() {
-        try (FileOutputStream writer = new FileOutputStream("source.data")) {
+    private static final String SOURCE_FILE = "source.data";
+    private static final String DEST_FILE = "destination.data";
+
+    public static void writerFunc(String filePath) {
+        try (FileOutputStream writer = new FileOutputStream(filePath)) {
             for (int i = 0; i <= 10; i++) {
                 writer.write(i);
             }
@@ -19,11 +22,25 @@ public class Ex4 {
 
     public static void copyFileBytes(String sourcePath, String destPath){
 
+        try(FileOutputStream writer2 = new FileOutputStream(destPath);
+            FileInputStream reader = new FileInputStream(sourcePath)){
+
+            int b;
+            while ((b = reader.read()) != -1) {
+                writer2.write(b);
+            }
+            System.out.println("File copied successfully.");
+
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 
 
     public static void main(String[] args) {
-
+  writerFunc(SOURCE_FILE);
+  copyFileBytes(SOURCE_FILE,DEST_FILE);
 
     }
 }
